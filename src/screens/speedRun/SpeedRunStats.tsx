e8ef4865ac77b1c05d0b266b90eabc6f148e02a9
malialtunsoy <altunsoymali@gmail.com>
2022-04-26 13:51:15 +0300

    stat screen fixes.

--- src/screens/speedRun/SpeedRunStats.tsx
+++ src/screens/speedRun/SpeedRunStats.tsx
@@ -1,34 +1,44 @@
+import { AntDesign } from '@expo/vector-icons'
 import { LinearGradient } from 'expo-linear-gradient'
 import React from 'react'
 import { View, StyleSheet, Pressable, Text } from 'react-native'
 
-const SpeedRunStats = ({navigation, route}) => {
-    const {correct, time} = route.params;
- const handleMainMenu =()=>{
-    navigation.navigate("SPEEDRUN_START", {goHome: true})
- }
+const SpeedRunStats = ({ navigation, route }) => {
+  const { correct, time } = route.params
+  const handleMainMenu = () => {
+    navigation.navigate('SPEEDRUN_START', { goHome: true })
+  }
   return (
     <View style={styles.frame}>
-        <Text style={styles.text}>Solid Stuff!</Text>
-        <Text style={styles.text}>You answered {correct} questions correctly.</Text>
-        <Text style={styles.text}>In {time} seconds!</Text>
-        <View style={styles.buttonPanel}>
+      <Text style={styles.text}>Solid Stuff!</Text>
+      <View style={styles.icon}>
+        <AntDesign name="checkcircle" size={150} color="green" />
+      </View>
 
+      <Text style={styles.text}>
+        You answered {correct} questions correctly
+      </Text>
+      <Text style={styles.text}>In {time} seconds!</Text>
+      <View style={styles.icon}>
+        <AntDesign name="Trophy" size={100} color="gold"/>
+      </View>
+      <Text style={styles.text}>Your record: {42 /*TODO*/}</Text> 
+      <View style={styles.buttonPanel}>
         <LinearGradient
-        colors={['#656CEE', '#ACB0FF']}
-        style={styles.ButtonOuter}
-      >
-        <Pressable
-          android_ripple={{ color: '#ccc' }}
-          style={styles.ButtonInner}
-          onPress={() => {
-            handleMainMenu()
-          }}
+          colors={['#656CEE', '#ACB0FF']}
+          style={styles.ButtonOuter}
         >
-          <Text style={styles.ButtonText}>Main Menu</Text>
-        </Pressable>
-      </LinearGradient>
-        </View>
+          <Pressable
+            android_ripple={{ color: '#ccc' }}
+            style={styles.ButtonInner}
+            onPress={() => {
+              handleMainMenu()
+            }}
+          >
+            <Text style={styles.ButtonText}>Main Menu</Text>
+          </Pressable>
+        </LinearGradient>
+      </View>
     </View>
   )
 }
@@ -36,19 +46,29 @@ const SpeedRunStats = ({navigation, route}) => {
 const styles = StyleSheet.create({
   frame: {
     flex: 1,
-    backgroundColor: '#303030'
+    backgroundColor: '#303030',
+    alignContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+    justifyContent: "space-around",
   },
-  text:{
-    color: "white",
+  text: {
+    color: 'white',
     fontSize: 30,
+    textAlign: "center",
   },
-  buttonPanel:{
-    width: "50%",
-    height: "10%",
-    alignSelf: "center",
+  buttonPanel: {
+    width: '50%',
+    height: '10%',
+    alignSelf: 'center'
+  },
+  icon: {
+    alignContent: "center",
+    justifyContent: "center",
+    alignItems: "center",
   },
   ButtonOuter: {
-      flex: 1,
+    flex: 1,
     overflow: 'hidden',
     borderRadius: 10,
     backgroundColor: 'blue',
